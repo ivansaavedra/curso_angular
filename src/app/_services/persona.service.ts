14d851cd41396055cad40f34a5ba3e820103705b
Ivan <ivan.saavedra@ciencias.unam.mx>
2020-11-23 10:01:45 -0600

    update persona service

--- src/app/_services/persona.service.ts
+++ src/app/_services/persona.service.ts
@@ -7,10 +7,20 @@ import { Persona } from '../_models/persona';
 })
 export class PersonaService {
 
+  API_URL = "https://cursoangularapi2020.herokuapp.com";
+
   constructor(private http: HttpClient) { }
 
   getPersonas(){
-    return this.http.get<Persona[]>("https://cursoangularapi2020.herokuapp.com/persona");
+    return this.http.get<Persona[]>(this.API_URL+"/persona");
+  }
+
+  getPersona(id: number){
+    return this.http.get<Persona>(this.API_URL+"/persona/"+id);
+  }
+
+  createPersona(persona: Persona){
+    return this.http.post(this.API_URL+"/persona/",persona);
   }
 
 }
