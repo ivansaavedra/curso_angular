2c00cb7c546e9d6c540d42e0fa99c02bb92ec9e5
Ivan <ivan.saavedra@ciencias.unam.mx>
2020-11-30 21:09:16 -0600

    update empleado component

--- src/app/_services/empleado.service.ts
+++ src/app/_services/empleado.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Empleado} from '../_models/empleado';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
+  
+  // API_URI = 'http://localhost:8080';
+  API_URI = 'https://cursoangularapi2020.herokuapp.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getEmpleados(){
+    return this.http.get<Empleado[]>(this.API_URI+'/empleado');
+  }
+
+  getEmpleado(id: number){
+    return this.http.get<Empleado>(this.API_URI+'/empleado/'+id);
+  }
+
+  creatEmpleado(empleado: Empleado){
+    console.log(empleado);
+    return this.http.post(this.API_URI+'/empleado',empleado);
+  }
+
+  updateEmpleado(empleado: Empleado){
+    return this.http.put(this.API_URI+'/empleado/'+empleado.id,empleado);
+  }
+
+  deleteEmpleado(id: number){
+    return this.http.delete(this.API_URI+'/empleado/'+id);
+  }
 }
--- src/app/components/empleado/empleado.component.ts
+++ src/app/components/empleado/empleado.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Empleado } from '../../_models/empleado';
+import { EmpleadoService } from '../../_services/empleado.service';
+
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+import Swal from 'sweetalert2'; 
+
+declare var $: any;
+
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
@@ -7,17 +16,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadoComponent implements OnInit {
 
-  constructor() { }
+  empleados: Empleado[] | any;
+  empleado: Empleado | any;
+  empleadoForm: FormGroup;
+  submitted = false;
+  modalTitle: string;
+  contrato: string;
+  fotografia: string;
+
+  constructor(private empleadoService: EmpleadoService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    // Inicie el formulario vacio
+    this.empleadoForm = this.formBuilder.group({
+      id: [''],
+      nombre: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      fecha_nacimiento: ['', Validators.required],
+      domicilio: ['', Validators.required],
+      no_empleado: ['', Validators.required],
+      fecha_ingreso: ['', Validators.required],
+      rfc: ['', Validators.required],
+      area: ['', Validators.required],
+      puesto: ['', Validators.required],
+      sueldo: ['', Validators.required],
+      fotografia: [''],
+      contrato: ['']
+    });
+
+    // Consulte lista personas
+    this.getEmpleados();
+
+    this.contrato = "contrato";
+    this.fotografia = "fotografia";
   }
 
   getEmpleados(){
-    
+    this.empleados = [];
+    this.empleadoService.getEmpleados().subscribe(
+      res => {
+        this.empleados = res;
+        console.log(this.empleados)
+      },
+      err => console.error(err)
+    )
   }
 
-  getEmpleado(id:number){
-    
+  // Consultar un empleado
+  getEmpleado(id){
+    this.empleado = null;
+    this.empleadoService.getEmpleado(id).subscribe(
+      res => {
+        this.empleado = res;
+      },
+      err => console.error(err)
+    )
+  }
+
+  // Eliminar un empleado
+  deleteEmpleado(id){
+    this.empleadoService.deleteEmpleado(id).subscribe(
+      res => {
+        this.getEmpleados();
+      },
+      err => console.error(err)
+    )
   }
 
 }
