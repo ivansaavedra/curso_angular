0d51e1670e232a79594f873d1848bafc342f4114
Ivan <ivan.saavedra@ciencias.unam.mx>
2020-12-03 16:39:04 -0600

    update spec.ts restringir-accesos

--- src/app/_services/restringir-accesos.service.spec.ts
+++ src/app/_services/restringir-accesos.service.spec.ts
@@ -2,15 +2,24 @@ import { TestBed } from '@angular/core/testing';
 
 import { RestringirAccesosService } from './restringir-accesos.service';
 
-describe('RestringirAccesosService', () => {
+import { RouterTestingModule } from '@angular/router/testing';
+
+fdescribe('RestringirAccesosService', () => {
   let service: RestringirAccesosService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule]
+    });
     service = TestBed.inject(RestringirAccesosService);
   });
 
-  it('should be created', () => {
+  fit('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  fit('token false', () => {
+    service.canActivate(null,null);
+    expect(JSON.parse(localStorage.getItem('token'))).toBeFalse();
   });
 });
